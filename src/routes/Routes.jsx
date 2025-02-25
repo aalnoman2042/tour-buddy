f41c31611fb4ce2d22bdc8f27651de9886d46930
aalnoman2042 <abdullahalnoman2042@gmail.com>
2025-02-25 18:15:16 +0600

    fixing

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,28 +1,21 @@
-import {
-    createBrowserRouter,
-  
-  } from "react-router-dom";
+import { createHashRouter } from "react-router-dom"
 import Main from "../layout/Main";
 import GuideProfile from "../pages/GuideProfile";
 import PaymentPage from "../pages/PaymentPage";
 
-
-
-  export const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Main></Main>,
-      children: [
-        {
-            path: '/',
-            element: <GuideProfile></GuideProfile>,
-
-        },
-        {
-          path:'/payment',
-          element: <PaymentPage></PaymentPage>
-        }
-      ]
-
-    },
-  ]);
+export const router = createHashRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <GuideProfile></GuideProfile>,
+      },
+      {
+        path: "/payment",
+        element: <PaymentPage></PaymentPage>,
+      },
+    ],
+  },
+]);
